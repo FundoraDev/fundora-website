3644aec86f23f21ae696dc0d26e945254b224add
Promise <promiseihunna@gmail.com>
2026-01-18 00:57:47 +0100

    Enhance 404 Not Found page with metadata and link to home

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,3 +1,11 @@
+import { Metadata } from "next";
+import Link from "next/link";
+
+export const metadata: Metadata = {
+  title: "404 | Not Found",
+  description: "This page could not be found.",
+};
+
 export default function NotFound() {
   return (
     <div className="flex flex-col items-center justify-center h-full gap-10 text-white">
@@ -6,7 +14,12 @@ export default function NotFound() {
         <div className="h-[5vh] border-white border"></div>
         <p className="text-sm font-semibold">This page could not be found.</p>
       </div>
-      <button className="border border-white p-2 rounded-lg text-[12px] font-light hover:font-semibold hover:border-2">Go Home</button>
+      <Link
+        href="/"
+        className="border border-white p-2 rounded-lg text-[12px] font-light hover:font-semibold hover:border-2"
+      >
+        Go Home
+      </Link>
     </div>
   );
 }
